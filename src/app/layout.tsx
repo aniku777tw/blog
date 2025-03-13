b8c2dfe8c6f0910e06835f0fa30eb239684f71c1
Hanz <e102777tw@gmail.com>
2025-03-13 21:49:27 +0800

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,11 @@ export default async function RootLayout({
 export async function generateMetadata({
   params,
 }: {
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
+  const { locale } = await params;
   const t = await getTranslations({
-    locale: params.locale,
+    locale,
     namespace: 'metadata',
   });
 
